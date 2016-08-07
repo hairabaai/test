ce36f67a06b84ea76e9e8fa6fcca1c091b483b48
hairabaai <xuetong.bupt@gmail.com>
2016-08-07 09:39:54 -0700

    [69. Sqrt(x)][Accepted]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -1,25 +1,16 @@
 public class Solution {
     public int mySqrt(int x) {
-        if (x==0 || x==1)
+        if (x <= 1)
             return x;
-            
-        int start = 0;
-        int end = x;
-        int mid = 0;
         
-        while (start<=end) {
-            mid = start + (end - start) / 2;
-            if (mid < x / mid) {
-                if (mid+1 > x / (mid+1))
-                    return mid;
-            start = mid + 1;
-            } else if (mid > x / mid) {
-                end = mid - 1;
-            } else {
-                return mid;
-            }
+        double lastY = 0;
+        double y = 1;
+        
+        while (y - lastY != 0){
+            lastY = y;
+            y = (lastY + x / lastY) / 2;
         }
-        return mid;
         
+        return (int) y;
     }
 }
