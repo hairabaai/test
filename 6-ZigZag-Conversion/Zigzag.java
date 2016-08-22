bbaea1e342890996fae1165e5e98dec80ac56047
hairabaai <xuetong.bupt@gmail.com>
2016-08-21 23:13:24 -0700

    [6. ZigZag Conversion][Accepted]

---
+++ 6-ZigZag-Conversion/Zigzag.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (s == null || s.length() == 0 || numRows < 0) {
+            return "";
+        }
+        if (numRows == 1) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        int size = 2 * numRows - 2;
+        
+        for (int i = 0; i < numRows; i++) {
+            for(int j = i; j < s.length(); j+=size) {
+                sb.append(s.charAt(j));
+                if (i != 0 && i != numRows-1){
+                    int tmp = j + size - 2*i;
+                    if(tmp < s.length()){
+                        sb.append(s.charAt(tmp));
+                    }
+                    
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
