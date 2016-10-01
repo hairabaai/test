fdb208b5abbd1f9ef4db78dbcd21ddb55717d540
hairabaai <xuetong.bupt@gmail.com>
2016-10-01 12:07:12 -0700

    [77. Combinations][Accepted]注意是i+1不是pos+1

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,28 +1,24 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result =  new ArrayList<List<Integer>>();
-        if(n==0 || k==0){
+        if(n <= 0 || k == 0 || n < k){
             return result;
         }
-        int[] a = new int[n];
-        for(int i = 0; i < n; i++){
-            a[i] = i+1;
-        }
+
         List<Integer> list = new ArrayList<>();
-        
-        helper(result, list, a, k, 0);
+        helper(result, list, n, k, 1);
         return result;
     }
     
-    private void helper(List<List<Integer>> result, List<Integer> list, int[] a, int k, int pos){
+    private void helper(List<List<Integer>> result, List<Integer> list, int n, int k, int pos){
         if(k == 0){
             result.add(new ArrayList<>(list));
             return;
         }
         
-        for(int i = pos; i < a.length; i++){
-            list.add(a[i]);
-            helper(result, list, a, k-1, i+1);
+        for(int i = pos; i < n+1; i++){
+            list.add(i);
+            helper(result, list, n, k-1, i+1);
             list.remove(list.size()-1);
         }
     }
